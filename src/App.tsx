918d62756f5b7fa6aff049a498eb2d6e64f39e3d
Trajan <trajan.bronner@specitec.com>
2022-01-31 23:14:31 +0100

    Init work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <div>
+            <a href={'#HTML-anchor'}>HTML</a>
+            <a href={'#CSS-anchor'}>CSS</a>
+            <a href={'#JS-anchor'}>JS</a>
+          </div>
+          <div>
+            <a href={'#Angular-anchor'}>Angular</a>
+          </div>
+          <div>
+            <a href={'#NET-anchor'}>.NET</a>
+          </div>
+          <div>
+            <a href={'#Shell-anchor'}>Shell</a>
+            <a href={'#Docker-anchor'}>Docker</a>
+          </div>
+        </nav>
       </header>
+
+      <div className="content">
+        <h1 id="HTML-anchor">HTML</h1>
+        <h1 id="CSS-anchor">CSS</h1>
+        <h1 id="JS-anchor">JS</h1>
+        <h1 id="Angular-anchor">Angular</h1>
+        <h1 id="NET-anchor">.NET</h1>
+        <h1 id="Shell-anchor">Shell</h1>
+        <h1 id="Docker-anchor">Docker</h1>
+      </div>
     </div>
   );
 }
