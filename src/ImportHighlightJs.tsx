921b502d4612b94c5b10dc20094c81551e257994
Trajan <trajan.bronner@specitec.com>
2022-02-06 22:08:24 +0100

    Add languages for highlight

--- src/ImportHighlightJs.tsx
+++ src/ImportHighlightJs.tsx
@@ -1,8 +1,22 @@
 import 'highlight.js/styles/stackoverflow-dark.css';
 import hljs from 'highlight.js/lib/core';
 import sql from 'highlight.js/lib/languages/sql';
+import shell from 'highlight.js/lib/languages/shell';
+import powershell from 'highlight.js/lib/languages/powershell';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+import csharp from 'highlight.js/lib/languages/csharp';
 
 hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('powershell', powershell);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('csharp', csharp);
 
 export function ImportHighlightJs() {
   return null;
