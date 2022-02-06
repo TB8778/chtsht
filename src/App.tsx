d79daf27721007f0e6588ee039ce2baa74c03760
Trajan <trajan.bronner@specitec.com>
2022-02-06 20:53:18 +0100

    Add accordion and code snippet formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import './App.css';
+import Code from './Code';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const code = `const App = props => {
+  return (
+    <div>
+      <h1> React App </h1>
+      <div>Awesome code</div>
+    </div>
+  );
+};
+`;
 
 function App() {
   return (
@@ -16,6 +29,7 @@ function App() {
           </div>
           <div>
             <a href={'#NET-anchor'}>.NET</a>
+            <a href={'#SQL-anchor'}>SQL</a>
           </div>
           <div>
             <a href={'#Shell-anchor'}>Shell</a>
@@ -32,6 +46,25 @@ function App() {
         <h1 id="NET-anchor">.NET</h1>
         <h1 id="Shell-anchor">Shell</h1>
         <h1 id="Docker-anchor">Docker</h1>
+        <h1 id="SQL-anchor">SQL</h1>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Code code={code} language="javascript" />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2a-content" id="panel2a-header">
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
